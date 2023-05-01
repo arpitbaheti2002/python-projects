482a1d5f6ca76d4a36b1124a8249131551fa3f83
Arpit Baheti <87475705+arpitbaheti2002@users.noreply.github.com>
2023-05-01 23:50:53 +0530

    added code for rock paper scissors

---
+++ Beginner/Rock paper scissors/main.py
@@ -0,0 +1,53 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+import random
+
+game_images=[rock, paper, scissors]
+
+user_choice = int(input("What do you choose? Type 0 for Rocks, 1 for Paper or 2 for Scissors."))
+
+if user_choice < 0 or user_choice > 2:
+  print("You typed an invalid number, you lose!")
+else:
+  print(game_images[user_choice])
+
+  computer_choice = random.randint(0,2)
+  print("Computer Choice:")
+  print(game_images[computer_choice])
+
+  if user_choice == computer_choice:
+    print("It is a draw")
+  elif user_choice == 0 and computer_choice == 2:
+    print("You win")
+  elif computer_choice == 0 and user_choice == 2:
+    print("You lose")
+  elif user_choice > computer_choice:
+    print("You win")
+  elif computer_choice > user_choice:
+    print("You lose")
+  
